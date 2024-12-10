adb521840f22343d5f753269c712473ac56cb25c
Adem Usta <austa@integralads.com>
2024-12-10 17:59:21 +0100

    wip: customer orders

--- tap_cegid/streams.py
+++ tap_cegid/streams.py
@@ -2,65 +2,32 @@
 
 from __future__ import annotations
 
-import typing as t
-from importlib import resources
-
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from singer_sdk import typing as th
 
 from tap_cegid.client import CegidStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = resources.files(__package__) / "schemas"
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
 
+class CustomerOrdersStream(CegidStream):
+    """Cegid customer orders stream."""
 
-class UsersStream(CegidStream):
-    """Define custom stream."""
+    name = "customer-orders"
+    path = "/api/customer-documents/v1"
 
-    name = "users"
-    path = "/users"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
         th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID",
+            "headers",
+            th.ObjectType(
+                th.Property(
+                    "active",
+                    th.BooleanType,
+                    description="Whether the customer order is active.",
+                ),
+                th.Property(
+                    "storeId",
+                    th.StringType,
+                    description="The store ID.",
+                ),
+            ),
+            description="Customer order headers.",
         ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
-
-
-class GroupsStream(CegidStream):
-    """Define custom stream."""
-
-    name = "groups"
-    path = "/groups"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
     ).to_dict()
